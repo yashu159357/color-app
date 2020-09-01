04a9cb1c871b8a9a98c76083a1e09e3cc4425bed
yashu159357 <yashu159357@gmail.com>
2020-09-01 20:17:52 +0530

    Updates

---
+++ precache-manifest.66a2d206ec3c65c9f667fc3f5be086a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cd2a2845f85e9dfa3609ab7f9a09b9",
+    "url": "/color-app/index.html"
+  },
+  {
+    "revision": "7eba7737b16b84cc919f",
+    "url": "/color-app/static/css/main.e441eed7.chunk.css"
+  },
+  {
+    "revision": "e6331d22694594570f75",
+    "url": "/color-app/static/js/2.f27bcce4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/color-app/static/js/2.f27bcce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eba7737b16b84cc919f",
+    "url": "/color-app/static/js/main.fc55e96d.chunk.js"
+  },
+  {
+    "revision": "f4e52d69a3f5595c502a",
+    "url": "/color-app/static/js/runtime-main.777304df.js"
+  }
+]);
